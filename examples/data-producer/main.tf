64dec429dd6158d8f730826d58b566b8d94a8e2a
robertsahlin <robert.sahlin@gmail.com>
2026-06-26 12:30:38 +0200

    fix: update provider version constraints to >= 0.1.0 to drop alpha dependency

--- examples/data-producer/main.tf
+++ examples/data-producer/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     supercargo = {
       source  = "supercargo-dev/supercargo"
-      version = ">= 0.0.1-alpha"
+      version = ">= 0.1.0"
     }
     google = {
       source  = "hashicorp/google"
