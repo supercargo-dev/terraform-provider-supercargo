70de9ebacdfe313bade21bcf3a7f2cf87490c2d4
robertsahlin <robert.sahlin@gmail.com>
2026-06-26 12:20:55 +0200

    fix: update provider source to HCP private registry

--- modules/data_product/versions.tf
+++ modules/data_product/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     supercargo = {
-      source  = "supercargo-dev/supercargo"
+      source  = "app.terraform.io/supercargo/supercargo"
       version = ">= 0.0.1-alpha.2"
     }
   }
