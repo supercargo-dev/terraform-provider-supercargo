581d7c86f704e176c01936096866645fbae970d3
robertsahlin <robert.sahlin@gmail.com>
2026-06-26 11:54:50 +0200

    fix: address PR review feedback
    Update provider source to supercargo-dev, fix version prefix in dataproduct.yaml, and format .gitignore correctly

--- examples/data-producer/main.tf
+++ examples/data-producer/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     supercargo = {
-      source  = "robertsahlin/supercargo"
+      source  = "supercargo-dev/supercargo"
       version = "~> 0.0.1"
     }
     google = {
